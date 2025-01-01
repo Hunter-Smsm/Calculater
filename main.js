386cefdbe5cfa47b75edd9770d18330be6fea0af
Hunter-Smsm <ziad35199@gmail.com>
2025-01-01 21:45:17 +0200

    update

--- main.js
+++ main.js
@@ -1,90 +1,31 @@
-if (localStorage.color) {
-    document.styleSheets[0].cssRules[8].style.setProperty(
-        "border-color",
-        `${localStorage.color}`
-    );
-    document.styleSheets[0].cssRules[9].style.setProperty(
-        "background-color",
-        `${localStorage.color}`
-    );
-    document.styleSheets[0].cssRules[10].style.setProperty(
-        "box-shadow",
-        `0 0 20px 2px ${localStorage.color}`
-    );
-    document.getElementById("color").value = localStorage.color;
-}
+const result = document.querySelector(".calculator .result");
+const allBtns = document.querySelectorAll(".calculator .buttons button");
 
-document.getElementById("color").onchange = function () {
-    localStorage.color = document.getElementById("color").value;
-    document.styleSheets[0].cssRules[8].style.setProperty(
-        "border-color",
-        `${localStorage.color}`
-    );
-    document.styleSheets[0].cssRules[9].style.setProperty(
-        "background-color",
-        `${localStorage.color}`
-    );
-    document.styleSheets[0].cssRules[10].style.setProperty(
-        "box-shadow",
-        `0 0 20px 2px ${localStorage.color}`
-    );
+const handelBtnClick = (e) => {
+	const btn = e.target;
+	btn.classList.add("dark");
+	const audio = document.createElement("audio");
+	audio.src = "./sounds/mixkit-arcade-game-jump-coin-216.wav";
+	audio.play();
+	setTimeout(() => {
+		btn.classList.remove("dark");
+	}, 200);
+	if (
+		btn.textContent !== "=" &&
+		btn.textContent !== "X" &&
+		btn.textContent !== "Ac"
+	) {
+		result.textContent += btn.textContent;
+	} else if (btn.textContent === "=") {
+		result.textContent = eval(result.textContent);
+	} else if (btn.textContent === "Ac") {
+		result.textContent = "";
+	} else {
+		result.textContent = result.textContent.substring(
+			0,
+			result.textContent.length - 1
+		);
+	}
 };
 
-let num1 = "",
-    num2 = "",
-    op = "";
-
-let used = false;
-
-function buttons() {
-    let audio = document.querySelector("audio").cloneNode();
-    audio.play();
-    if (this.innerHTML === "=") {
-        if (op === "+") {
-            num1 = +num1 + +num2;
-        } else if (op === "-") {
-            num1 -= num2;
-        } else if (op === "X") {
-            num1 *= num2;
-        } else if (op === "/") {
-            num1 /= num2;
-        }
-        (num2 = ""), (op = "");
-        used = true;
-    } else if (this.innerHTML === "Reset") {
-        num1 = num2 = op = "";
-    } else if (
-        this.innerHTML === "+" ||
-        this.innerHTML === "-" ||
-        this.innerHTML === "X" ||
-        this.innerHTML === "/"
-    ) {
-        if (num1 !== "") {
-            op = this.innerHTML;
-        }
-    } else if (op === "" && used === false) {
-        num1 += this.innerHTML;
-    } else if (op === "") {
-        num1 = "";
-        used = false;
-        num1 += this.innerHTML;
-    } else {
-        num2 += this.innerHTML;
-    }
-    document.querySelector(".result span").innerHTML = `${num1} ${op} ${num2}`;
-    if (!this.classList.contains("op")) {
-        this.classList.add("btnc");
-        setTimeout(() => {
-            this.classList.remove("btnc");
-        }, 350);
-    } else {
-        this.classList.add("opc");
-        setTimeout(() => {
-            this.classList.remove("opc");
-        }, 350);
-    }
-}
-
-document.querySelectorAll(".btn").forEach(function (el) {
-    el.onclick = buttons;
-});
+allBtns.forEach((btn) => btn.addEventListener("mousedown", handelBtnClick));
